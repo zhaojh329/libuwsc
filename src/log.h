ee210662c5e13b76dab2feb76d8869ba424c61ee
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-01-10 23:34:43 +0800

    Optimize code
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- src/log.h
+++ src/log.h
@@ -18,25 +18,19 @@
 #ifndef _LOG_H
 #define _LOG_H
 
-#include <syslog.h>
-#include "config.h"
+#include <libubox/ulog.h>
 
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 /*
  * Use the syslog output log and include the name and number of rows at the call
  */
-#define uwsc_log(priority, format...) __uwsc_log(__FILENAME__, __LINE__, priority, format)
+#define uwsc_log(priority, fmt...) __uwsc_log(__FILENAME__, __LINE__, priority, fmt)
 
-#if (UWSC_DEBUG)
-#define uwsc_log_debug(format...)     uwsc_log(LOG_DEBUG, format)
-#else
-#define uwsc_log_debug(format...)
-#endif
+#define uwsc_log_debug(fmt...)     uwsc_log(LOG_DEBUG, fmt)
+#define uwsc_log_info(fmt...)      uwsc_log(LOG_INFO, fmt)
+#define uwsc_log_err(fmt...)       uwsc_log(LOG_ERR, fmt)
 
-#define uwsc_log_info(format...)      uwsc_log(LOG_INFO, format)
-#define uwsc_log_err(format...)       uwsc_log(LOG_ERR, format)
-
-void  __uwsc_log(const char *filename, int line, int priority, const char *format, ...);
+void  __uwsc_log(const char *filename, int line, int priority, const char *fmt, ...);
 
 #endif
